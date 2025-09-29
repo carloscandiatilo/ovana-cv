a0396424474532f34e4593e0a3632901527ebdca
carloscandiatilo <carlos.97pedro@gmail.com>
2025-09-29 14:39:54 +0100

    fix dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    public function getWidgets(): array
+    {
+        return [
+            \App\Filament\Widgets\StatsOverview::class,
+        ];
+    }
+
+    public function getHeaderWidgets(): array
+    {
+        return [
+            // vazio → remove "Bem-vindo" e "Filament/Documentação"
+        ];
+    }
+}
